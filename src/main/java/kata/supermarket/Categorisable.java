255dee646436db156e8024cc7d2b2e4bede62aa4
pdrygas <drygaspawel@gmail.com>
2020-09-04 12:58:13 +0200

    The notion of category will allow for discounts applying to ranges of products e.g. diary, vegetables.

---
+++ src/main/java/kata/supermarket/Categorisable.java
@@ -0,0 +1,5 @@
+package kata.supermarket;
+
+interface Categorisable {
+    Category getCategory();
+}
---
+++ src/main/java/kata/supermarket/Category.java
@@ -0,0 +1,12 @@
+package kata.supermarket;
+
+/**
+ * Represents a range of products and forms a one-to-many relationship with them.
+ */
+class Category {
+    private final String name;
+
+    Category(String name) {
+        this.name = name;
+    }
+}
